8c3c82d2a7685256d95f4566742b923f9d2dddc6
Andres Moreno <andresmorenomora.06@gmail.com>
2016-07-05 18:30:49 -0400

    Creacion de formulario html

--- formulario.php
+++ formulario.php
@@ -4,6 +4,10 @@
 	<title>Procesar Archivos</title>
 </head>
 <body>
-
+	<form action="guardar.php" method="post">
+		<input type="text" name="nombre"> <br/><br/>
+		<textarea name="comentario"></textarea><br/><br/>
+		<input type="submit" value="Guardar">
+	</form>
 </body>
 </html>
